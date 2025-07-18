8d0c8a94509436d7fe9c47d9682a826d7453a610
Ernesto <dev.ejbr@gmail.com>
2025-07-18 11:59:07 -0400

    feat: Enhance AdminPanel with product management functions and update product data in db.json

--- Frontend/src/components/AdminPanel.jsx
+++ Frontend/src/components/AdminPanel.jsx
@@ -1,5 +1,38 @@
+
+
 const AdminPanel = () => {
-  return <div>Admin Panel</div>;
+
+  const SERVER = import.meta.env.VITE_SERVER_URL;
+
+
+  const handleAddProduct = async () => {
+    
+  };
+
+  const handleEditProduct = async () => {
+    // Lógica para editar un producto
+  };
+
+  const handleDeleteProduct = async () => {
+    // Lógica para eliminar un producto
+  };
+
+  return (
+    <>
+      <h1>Panel de administrador</h1>
+      <p>
+        Bienvenido al panel de administración. Aquí puedes gestionar usuarios,
+        ver estadísticas y más.
+      </p>
+
+      <section>
+        <h2>Gestion de Productos</h2>
+        <button onClick={handleAddProduct}>Agregar Producto</button>
+        <button onClick={handleEditProduct}>Editar Producto</button>
+        <button onClick={handleDeleteProduct}>Eliminar Producto</button>
+      </section>
+    </>
+  );
 };
 
 export default AdminPanel;
